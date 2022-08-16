8142c83e11a6f1812802334c46c62ac3218a9a16
varoon <developer.designer18@gmail.com>
2022-08-16 17:22:06 +0530

    changes

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+#test purpose:-
+class HelloApiView(APIView):
+    def get(self, request, format=None):
+        an_apiview = [
+            'hey! hows goin'
+        ]
+
+        return Response({'message': 'hello!', 'an_apiview': an_apiview})
