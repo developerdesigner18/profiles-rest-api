d41f0159ff1ad905ed0d62c3b0ff02159dc556e6
varoon <developer.designer18@gmail.com>
2022-08-18 12:19:28 +0530

    changes

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -7,6 +7,7 @@ from rest_framework import viewsets
 from profiles_api import models
 from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
+from rest_framework import filters
 
 # Create your views here.
 #test purpose:-
@@ -83,3 +84,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
